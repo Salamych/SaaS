7c4f424cc7a5e18ad778bdbcbe6d41bf7268184f
Ivan Volkov <volkov19@mail.ru>
2024-11-01 20:42:00 +0400

    Start SaaS

---
+++ config/app.js
@@ -0,0 +1,18 @@
+const isProd = process.argv.includes("--production");
+const isDev = !isProd;
+
+const app = {
+  isProd: isProd,
+  isDev: isDev,
+
+  htmlmin: {collapseWhitespace: isProd},
+
+  webpack: {mode: isProd ? "production" : "development"},
+
+  imagemin: {verbose: true},
+
+  fonter: {formats: ["ttf", "woff", "eot", "svg"]}
+
+}
+
+export {app};
